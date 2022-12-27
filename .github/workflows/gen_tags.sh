899ed0f2a184eb937e70c5c257df631d4936c390
Paul <paul70079@gmail.com>
2022-12-27 15:51:03 +0100

    Replace CI pipeline

---
+++ .github/workflows/gen_tags.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+IMAGE_NAME=$(echo $REPO_NAME | sed 's/docker-//g')
+
+VERSION=$(grep '^FROM ' Dockerfile | sed -E 's/.*:(.*)/\1/g')
+
+VERSION_TAGS+=(latest)
+VERSION_TAGS+=("${VERSION}")
+
+BASES+=(${IMAGE_NAME})
+BASES+=(ghcr.io/${IMAGE_NAME})
+
+for b in "${BASES[@]}"
+do
+   for t in "${VERSION_TAGS[@]}"
+   do
+      TAGS+=( "${b}:${t}" )
+   done
+done
+
+echo "TAGS<<EOF"
+printf '%s\n' ${TAGS[@]}
+echo "EOF"
+
+echo "TAGS<<EOF" >> $GITHUB_ENV
+printf '%s\n' ${TAGS[@]} >> $GITHUB_ENV
+echo "EOF" >> $GITHUB_ENV
